307d615de3b9b5d80d77323ead9df4b04c6c5571
rawcptr <59281378+rawcptr@users.noreply.github.com>
2025-09-17 02:35:00 +0530

    refactor(ctx): add generics over dtype and a checked_get method

--- src/context.rs
+++ src/context.rs
@@ -7,11 +7,17 @@ pub struct Context<D = f32> {
     pub tensors: HashMap<Id, TensorData<D>>,
 }
 
-impl Context {
+impl<D> Context<D> {
     pub fn new() -> Self {
         Self {
             tensors: HashMap::new(),
         }
+    }
+
+    pub fn checked_get(&self, id: &Id) -> &TensorData<D> {
+        self.tensors
+            .get(id)
+            .unwrap_or_else(|| panic!("tensor({id:?}) was not found in context."))
     }
 }
 
