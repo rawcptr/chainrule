11113062ccc3d82729288e8e1c9cde6f1c017199
rawcptr <59281378+rawcptr@users.noreply.github.com>
2025-09-17 00:04:27 +0530

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+//! API.
+//!
+//! ```rust
+//! use chainrule::trace_fn;
+//! use chainrule::Tensor
+//! use ndarray::array;
+//!
+//! #[trace]
+//! fn multiply(x: Tensor:, y: Tensor) -> Tensor {
+//!     x * y + 1.0
+//! }
+//!
+//! fn main() {
+//!     let a = array![1., 2., 3.];
+//!     let b = array![4., 5., 6.];
+//!     let f = trace_fn(multiply);
+//!     f.eval()(&a, &b);
+//!     f.grad()(&a, &b);
+//!     f.grad().grad()(&a, &b);
+//! }
+//! ```
