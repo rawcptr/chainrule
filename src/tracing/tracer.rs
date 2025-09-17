40606051804b6359021d8dfe9126f53b761ee97d
rawcptr <59281378+rawcptr@users.noreply.github.com>
2025-09-17 23:26:50 +0530

    feat(tracing): add dummy operator impls for tracers

--- src/tracing/tracer.rs
+++ src/tracing/tracer.rs
@@ -1,5 +1,7 @@
 use crate::identity::Id;
 
+use std::ops::{Add, Mul, Neg, Sub};
+
 pub type TensorData<T = f32> = ndarray::ArrayD<T>;
 pub type Tensor = Tracer;
 
@@ -14,5 +16,33 @@ impl Tracer {
     }
     pub fn id(&self) -> Id {
         self.id
+    }
+}
+
+impl Add for Tracer {
+    type Output = Tracer;
+    fn add(self, _rhs: Tracer) -> Tracer {
+        panic!("dummy Add operator – only allowed inside #[trace] functions")
+    }
+}
+
+impl Sub for Tracer {
+    type Output = Tracer;
+    fn sub(self, _rhs: Tracer) -> Tracer {
+        panic!("dummy Sub operator – only allowed inside #[trace] functions")
+    }
+}
+
+impl Mul for Tracer {
+    type Output = Tracer;
+    fn mul(self, _rhs: Tracer) -> Tracer {
+        panic!("dummy Mul operator – only allowed inside #[trace] functions")
+    }
+}
+
+impl Neg for Tracer {
+    type Output = Tracer;
+    fn neg(self) -> Tracer {
+        panic!("dummy Neg operator – only allowed inside #[trace] functions")
     }
 }
