e0e81a55f8e882d9f096afa23595dbd66315adc3
rawcptr <59281378+rawcptr@users.noreply.github.com>
2025-09-19 14:25:37 +0530

    feat(tracing): breaking: change output type from a vector to a n-tuple.

--- src/tracing/function.rs
+++ src/tracing/function.rs
@@ -1,4 +1,8 @@
-use crate::{Floating, context::Context, graph::Graph, identity::Id, tracing::TensorData};
+use std::collections::HashMap;
+
+use crate::{
+    Floating, context::Context, graph::Graph, identity::Id, ops::Const, tracing::TensorData,
+};
 
 #[derive(Debug, Clone)]
 pub struct TraceableFn<D: Floating> {
@@ -12,32 +16,85 @@ pub trait EvalArgs<D: Floating> {
 }
 
 impl<D: Floating + 'static> TraceableFn<D> {
-    pub fn eval<T>(&self) -> impl Fn(T) -> TensorData<D>
+    fn run<T: EvalArgs<D>, O: EvalOutputs<D>>(&self, args: T) -> O {
+        let packed = args.pack();
+        let mut ctx = Context::<D>::new();
+
+        for (id, val) in self.inputs.iter().zip(packed.into_iter()) {
+            ctx.tensors.insert(*id, val);
+        }
+
+        for op in &self.graph.nodes {
+            op.eval(&mut ctx);
+        }
+
+        O::from_vec(
+            self.outputs
+                .iter()
+                .map(|id| ctx.checked_get(id).clone())
+                .collect(),
+        )
+    }
+    /// plucks a single
+    pub fn eval<T, O>(&self) -> impl Fn(T) -> O
     where
         T: EvalArgs<D>,
+        O: EvalOutputs<D>,
     {
-        move |args: T| {
-            let packed = args.pack();
-            let mut ctx = Context::<D>::new();
+        move |args: T| self.run(args)
+    }
 
-            for (id, val) in self.inputs.iter().zip(packed.into_iter()) {
-                ctx.tensors.insert(*id, val);
-            }
-
-            for op in &self.graph.nodes {
-                op.eval(&mut ctx);
-            }
-
-            ctx.checked_get(&self.outputs[0]).clone()
-        }
+    pub fn grad(&self) -> Self {
+        todo!("gradients are not implemented yet.")
     }
 }
 
-macro_rules! as_ref_ty {
-    ($_:ident, $lt:lifetime, $D:ident) => {
-        &$lt TensorData<$D>
-    };
+pub trait EvalOutputs<D> {
+    fn from_vec(f: Vec<TensorData<D>>) -> Self;
 }
+
+mod macros {
+    use super::{EvalArgs, EvalOutputs, Floating, TensorData};
+    macro_rules! as_owned_ty {
+        ($_:ident, $D:ident) => {
+            TensorData<$D>
+        };
+    }
+
+    macro_rules! as_ref_ty {
+        ($_:ident, $D:ident) => {
+            &TensorData<$D>
+        };
+    }
+
+    macro_rules! reverse_order {
+    // base case
+    (($last:ident), $vec:ident) => {
+        let $last = $vec.pop().unwrap();
+    };
+
+    // recursive case
+    (($first:ident, $($rest:ident),+), $vec:ident) => {
+        reverse_order!(($($rest),+), $vec);
+        let $first = $vec.pop().unwrap();
+    };
+    }
+
+    macro_rules! impl_eval_outputs {
+        ( $( $len:literal => ( $( $name:ident ),+ ) ),+ $(,)? ) => {
+            $(
+                // #[expect(unused_parens, reason = "macro complains")]
+                impl<D: Floating> EvalOutputs<D>
+                for ( $( as_owned_ty!($name, D) ),+ ,) {
+                    fn from_vec(mut f: Vec<TensorData<D>>) -> Self {
+                        assert_eq!(f.len(), $len);
+                        reverse_order!(($($name),+), f);
+                        ( $( $name ),+ ,)
+                    }
+                }
+            )+
+        };
+    }
 
     macro_rules! impl_eval_args {
         ( $( $len:literal => ( $( $name:ident ),+ ) ),+ $(,)? ) => {
@@ -55,15 +112,23 @@ macro_rules! as_ref_ty {
         };
     }
 
-impl_eval_args! {
-    1  => (a),
-    2  => (a,b),
-    3  => (a,b,c),
-    4  => (a,b,c,d),
-    5  => (a,b,c,d,e),
-    6  => (a,b,c,d,e,f),
-    // 7  => (a,b,c,d,e,f,g),
-    // 8  => (a,b,c,d,e,f,g,h),
-    // 9  => (a,b,c,d,e,f,g,h,i),
-    // 10 => (a,b,c,d,e,f,g,h,i,j)
+    impl_eval_args! {
+        1  => (a),
+        2  => (a,b),
+        3  => (a,b,c),
+        4  => (a,b,c,d),
+        5  => (a,b,c,d,e),
+        6  => (a,b,c,d,e,f),
+        7  => (a,b,c,d,e,f,g),
+    }
+
+    impl_eval_outputs! {
+        1  => (a),
+        2  => (a,b),
+        3  => (a,b,c),
+        4  => (a,b,c,d),
+        5  => (a,b,c,d,e),
+        6  => (a,b,c,d,e,f),
+        7  => (a,b,c,d,e,f,g)
+    }
 }
