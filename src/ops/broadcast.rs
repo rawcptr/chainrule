2538c5c688da73f3d287c28ec041846cc934fdae
rawcptr <59281378+rawcptr@users.noreply.github.com>
2025-09-19 21:17:37 +0530

    refactor(ops): add "Like" node to defer shapes to eval time

--- src/ops/broadcast.rs
+++ src/ops/broadcast.rs
@@ -1,7 +1,7 @@
 use crate::{
     Floating, Id, Tracer,
     context::Context,
-    ops::{Op, sum::Sum},
+    ops::{Op, sum::ReduceToLike},
     tracing::session::TraceSession,
 };
 
@@ -9,7 +9,7 @@ use crate::{
 pub struct Broadcast {
     inp: Id,
     out: Id,
-    inp_shape: Vec<usize>,
+    // inp_shape: Vec<usize>,
     target_shape: Vec<usize>,
 }
 
@@ -18,22 +18,12 @@ impl Broadcast {
         inp: Id,
         out: Id,
         target: impl Into<Vec<usize>>,
-        inp_shape: impl Into<Vec<usize>>,
     ) -> Self {
         Self {
             inp,
             out,
-            inp_shape: inp_shape.into(),
             target_shape: target.into(),
         }
-    }
-    pub fn boxed(
-        inp: Id,
-        out: Id,
-        target: impl Into<Vec<usize>>,
-        inp_shape: impl Into<Vec<usize>>,
-    ) -> Box<Self> {
-        Box::new(Self::new(inp, out, target, inp_shape))
     }
 }
 
@@ -53,33 +43,10 @@ impl<D: Floating> Op<D> for Broadcast {
     }
 
     fn vjp(&self, g: &mut crate::Graph<D>, out_grads: &[Id]) -> Option<Vec<Id>> {
+        // d/dx broadcast(x -> target) = reduce_to_like(og, like=x)
         let grad_y = *out_grads.first()?;
         let out = g.fresh();
-        let mut axes_to_reduce = Vec::new();
-        let target_rank = self.target_shape.len();
-        let inp_rank = self.inp_shape.len();
-
-        for i in 0..(target_rank - inp_rank) {
-            axes_to_reduce.push(i);
-        }
-
-        for i in 0..inp_rank {
-            let inp_dim = self.inp_shape.get(i)?;
-            let target_dim = self.target_shape.get(i + (target_rank - inp_rank))?;
-            if *inp_dim == 1 && *target_dim > 1 {
-                axes_to_reduce.push(i + (target_rank - inp_rank));
-            }
-        }
-
-        let sum = Box::new(Sum::new(
-            grad_y,
-            out,
-            axes_to_reduce,
-            &*self.target_shape,
-            true,
-        ));
-
-        g.push(sum);
+        g.push(Box::new(ReduceToLike::new(grad_y, self.inp, out)));
         Some(vec![out])
     }
 
@@ -96,12 +63,59 @@ impl<D: Floating + 'static> TraceSession<'_, D> {
     #[must_use]
     pub fn broadcast(&mut self, t: Tracer, shape: Vec<usize>) -> Tracer {
         let out = self.g.fresh();
-        self.emit(Broadcast::new(t.id(), out, shape, t.shape()), out)
+        // self.emit(Broadcast::new(t.id(), out, shape, t.shape()), out)
+        self.emit(Broadcast::new(t.id(), out, shape), out)
     }
 }
 
 impl Tracer {
     pub fn broadcast(&self, _: Tracer, _: Vec<usize>) -> Tracer {
         panic!("dummy operation - only allowed inside #[trace] function")
+    }
+}
+
+// Broadcast to the runtime shape of `like`.
+#[derive(Debug, Clone)]
+pub struct BroadcastLike {
+    inp: Id,
+    like: Id,
+    out: Id,
+}
+
+impl BroadcastLike {
+    pub fn new(inp: Id, like: Id, out: Id) -> Self {
+        Self { inp, like, out }
+    }
+}
+
+impl<D: Floating> Op<D> for BroadcastLike {
+    fn name(&self) -> &'static str {
+        "broadcast_like"
+    }
+
+    fn eval(&self, ctx: &mut Context<D>) {
+        let x = ctx.checked_get(&self.inp).clone();
+        let like = ctx.checked_get(&self.like);
+        let shape = like.shape().to_vec();
+        let y = x
+            .broadcast(shape)
+            .expect("broadcast_like: incompatible shapes")
+            .to_owned();
+        ctx.tensors.insert(self.out, y);
+    }
+
+    fn vjp(&self, g: &mut crate::Graph<D>, out_grads: &[Id]) -> Option<Vec<Id>> {
+        // grad wrt x = reduce_to_like(og, like=x)
+        let og = *out_grads.first()?;
+        let out = g.fresh();
+        g.push(Box::new(ReduceToLike::new(og, self.inp, out)));
+        Some(vec![out])
+    }
+
+    fn inputs(&self) -> Vec<Id> {
+        vec![self.inp, self.like]
+    }
+    fn outputs(&self) -> Vec<Id> {
+        vec![self.out]
     }
 }
